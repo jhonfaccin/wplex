2aca0b294df3c9f1208c14f941607fe9774492de
jhon <jhonfaccin@gmail.com>
2024-04-21 03:01:28 -0300

    create module to read and create file csv

---
+++ src/csvProcessor.js
@@ -0,0 +1,49 @@
+const csv = require('csv-parser');
+const csvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
+
+const readFile = async (file,filterFunction) => {
+    return new Promise((resolve, reject) => {
+        const dataCSV = [];
+        fs.createReadStream(file)
+            .pipe(csv())
+            .on('data', (data) => {
+                if (filterFunction && filterFunction(data)) {
+                    dataCSV.push(data);
+                } else if (filterFunction === undefined){
+                    dataCSV.push(data);
+                }
+            })
+            .on('end', () => {
+                resolve(dataCSV);
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
+}
+
+const createFileCSV = async (data, header) => {
+    const writer = csvWriter({
+        path: 'output.csv',
+        header: header
+    });
+    const writeStream = fs.createWriteStream('output.csv');
+
+    writer.writeRecords(data) 
+        .then(() => {
+            console.log('CSV file created successfully');
+        })
+        .catch((err) => {
+            throw Error('Error writing CSV:', err);
+        });
+    writeStream.end();
+
+}
+
+module.exports = {
+    csvProcessor: {
+        readFile,
+        createFileCSV
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { csvProcessor } = require("./csvProcessor");
+const geolib = require('geolib');
+
+const filterEventsByRadius = (event) => {
+    if (!event || !event.payload) {
+        return false;
+    }
+    const parts = event.payload.split(',');
+    let coordenate = {};
+    coordenate.latitude = parseFloat(parts[2]);
+    coordenate.longitude = parseFloat(parts[3].replace('<', ''));
+    const distance = geolib.getPreciseDistance(referenceCoordinate, coordenate, 0.01);
+    return distance < 50;
+}
+
+const main = async () => {
+    referenceCoordinate = { latitude: parseFloat("-23.70041"), longitude: parseFloat("-046.53713") };
+    let dataCSV = [];
+    try {
+        dataCSV = await csvProcessor.readFile("eventlog.csv", filterEventsByRadius);
+    } catch (error) {
+        console.log(error);
+    }
+    // const filteredData = filterEventsByRadius(dataCSV);
+
+    const header = [
+        { id: 'device', title: 'Device' },
+        { id: 'prfix', title: 'Prefix' },
+        { id: 'instant', title: 'Instant' },
+        { id: 'payload', title: 'Payload' },
+        { id: 'company', title: 'Company' }]
+    console.log(dataCSV)
+};
+
+main();
